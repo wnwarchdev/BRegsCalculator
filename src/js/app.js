0b173f84af7b8e010437b1d44394eb1cfca19e12
Wojtek Nowak <billnewmann@gmail.com>
2023-09-23 18:32:14 +0100

    add basic logic in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,37 @@
-//add classnames
+//ADD CLASSNAMES
 
-//add occupancyCalculator
+//ADD OCCUPANCYCALCULATOR
 
-//add maleNum and femaleNum variables
+let occupancy;
+occupancy = 100;
 
-//add toiletCalculator
+//occupancy = prompt()
 
-//add blockArranger
+
+//ADD MALENUM AND FEMALENUM VARIABLES
+
+let maleNum;
+let femaleNum;
+let maleRatio;
+let femaleRatio;
+
+defaultRatio = 60
+maleRatio = defaultRatio
+femaleRatio = defaultRatio
+
+//maleRatio = prompt()
+//femaleRatio = prompt()
+
+maleNum = occupancy * maleRatio / 100
+femaleNum = occupancy * femaleRatio / 100
+
+// console.log('occupancy is: ', occupancy)
+// console.log('ratios are: Male(',maleRatio,') and Female(', femaleRatio, ')')
+// console.log('maleNum is: ',maleNum)
+// console.log('femaleNum is: ',femaleNum)
+
+
+
+//ADD TOILETCALCULATOR
+
+//ADD BLOCKARRANGER
