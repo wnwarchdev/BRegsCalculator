06d1fc513826589334bea867c0340b210c58758f
Wojtek Nowak <billnewmann@gmail.com>
2023-09-24 22:37:25 +0100

    further develop calculator logic; started to develop calculator for alternative provision

--- src/js/app.js
+++ src/js/app.js
@@ -28,6 +28,8 @@ let maleNum;
 let femaleNum;
 let maleRatio;
 let femaleRatio;
+let maleToilet;
+let femaleToilet;
 
 let defaultRatio = 60
 maleRatio = defaultRatio
@@ -48,6 +50,23 @@ femaleNum = occupancy * femaleRatio / 100
 
 //ADD TOILETCALCULATOR
 
+function calcItems(num){
+    let result
+    let over100 = 0
+    result = num<=5 ? 1 : num<=15 ? 2 : num<=30 ? 3 : num<=45 ? 4 : num<=60 ? 5: num<=75 ? 6 : num<=90 ? 7 : 8
+    over100 = num-100 > 0 ? Math.ceil((num - 100)/25) : 0 
+    //console.log('result of over: ',over100)
+    result = result + over100
+return result}
+
+function calcAltItems(num){
+    let result
+    let over100 = 0
+    result = num<=15 ? [1,1,1] : num<=30 ? [2,1,2] : num<=45 ? [2,2,2] : num<=60 ? [3,2,3] : num<=75 ? [3,3,3] : num<=90 ? [4,3,4] : [4,4,4]
+    over100 = num-100 > 0 ? Math.ceil((num - 100)/50) : 0 
+    //result = result + over100
+return result}
+
 calcButton.addEventListener('click', function(){
     console.log('clicked calc');
     maleRatio = maleRatioAtt.value
@@ -58,12 +77,21 @@ calcButton.addEventListener('click', function(){
     //console.log(occupancy)
     maleNum = occupancy * maleRatio / 100
     femaleNum = occupancy * femaleRatio / 100
+
+
+    maleToilet = calcItems(maleNum)
+    femaleToilet = calcItems(femaleNum)
+
+
+
     //console.log(maleNum, femaleNum)
     resultParagraph.innerHTML = null
     resultParagraph.innerHTML = `<p>Based on ${occupancy} occupants number, at ${maleRatio}/${femaleRatio} ratio</p>
-    <p>male toilet number is: ${maleNum}</p> <p>female toilet number is: ${femaleNum}</p>`
+    <p>male occupancy number is: ${maleNum} and female occupancy number is: ${femaleNum}</p>
+    <p>male toilet number is: ${maleToilet}</p> <p>female toilet number is: ${femaleToilet}</p>`
 
-
+    calcAltItems(maleNum)
+    console.log(calcAltItems(maleNum))
 
   });
 
