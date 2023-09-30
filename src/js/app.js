ab6d603c7f885b37e4cce007f5bb156c669bd680
Wojtek Nowak <billnewmann@gmail.com>
2023-09-30 22:16:50 +0100

    add runCalcs() function and refactor listeners accordingly; add number validation to inputs in html

--- src/js/app.js
+++ src/js/app.js
@@ -14,7 +14,8 @@ const maleRatioAtt = document.getElementById('maleRatio');
 const femaleRatioAtt = document.getElementById('femaleRatio');
 const occNumAtt = document.getElementById('occNum');
 const resultParagraph = document.getElementById('resultsPara');
-const arrangeParagraph = document.getElementById('arrangePara');
+const arrangeParagraphMale = document.getElementById('arrangeMale');
+const arrangeParagraphFemale = document.getElementById('arrangeFemale');
 
 
 
@@ -62,24 +63,11 @@ femaleRatio = defaultRatio;
 function calcItems(num, uri){
   let result;
 
-  //let over100 = 0
   if (uri == true) {
-
     result = num<=15 ? [1,1,1] : num<=30 ? [2,1,2] : num<=45 ? [2,2,2] : num<=60 ? [3,2,3] : num<=75 ? [3,3,3] : num<=90 ? [4,3,4] : num<=100 ? [4,4,4] : [4 , 4 + Math.ceil((num - 100)/50), 4 + Math.ceil((num - 100)/50)];
-    //over100 = num-100 > 0 ? Math.ceil((num - 100)/50) : 0 
-
-
-
   } else {
-
     result = num<=5 ? [1,0,1] : num<=15 ? [2,0,2] : num<=30 ? [3,0,3] : num<=45 ? [4,0,4] : num<=60 ? [5,0,5]: num<=75 ? [6,0,6] : num<=90 ? [7,0,7] : num<=100 ? [8,0,8] : [8 + Math.ceil((num - 100)/25),0,8 + Math.ceil((num - 100)/25)];
-    //over100 = num-100 > 0 ? Math.ceil((num - 100)/25) : 0 
-
   }
-
-
-  //console.log('result of over: ',over100)
-  //result = result + over100
   return result;}
 
 // function calcItems(num){
@@ -99,6 +87,45 @@ function calcItems(num, uri){
 //     //result = result + over100
 //     //console.log(num)
 // return result}
+
+function runCalcs(){
+
+  maleRatio = maleRatioAtt.value;
+  femaleRatio = femaleRatioAtt.value;
+  occupancy = occNumAtt.value;
+  maleNum = Math.ceil(occupancy * maleRatio / 100);
+  femaleNum = Math.ceil(occupancy * femaleRatio / 100);
+  maleToilet = calcItems(maleNum,uriBool)[0];
+  maleUrinal = calcItems(maleNum,uriBool)[1];
+  femaleToilet = calcItems(femaleNum)[0];
+  maleWashbasin = calcItems(maleNum,uriBool)[2];
+  femaleWashbasin = calcItems(femaleNum)[2];
+  // maleToiletAlt = calcAltItems(maleNum)[0]
+  // maleWashbasinAlt = calcAltItems(maleNum)[2]
+  // maleUrinal = calcAltItems(maleNum)[1]
+
+    
+    
+    
+  //console.log(femaleToilet)
+
+
+  //console.log(maleNum, femaleNum)
+  resultParagraph.innerHTML = null;
+  resultParagraph.innerHTML = `<p>Based on ${occupancy} occupants number, at ${maleRatio}/${femaleRatio} ratio</p>
+    <p>male occupancy number is: ${maleNum} and female occupancy number is: ${femaleNum}</p>
+    ${uriBool==false ? `<p>male toilet requires ${maleToilet} WCs and ${maleWashbasin} washbasins</p>` : `<p>male toilet requires ${maleToilet} WCs, ${maleUrinal} urinals and ${maleWashbasin} washbasins</p>` }
+    <p>female toilet requires ${femaleToilet} WCs and ${femaleWashbasin} washbasins</p>`;
+
+  arrangeParagraphMale.innerHTML = null;
+  arrangeParagraphFemale.innerHTML = null;
+  arrangeParagraphMale.innerHTML = arranger(maleToilet,sepBool);
+  arrangeParagraphFemale.innerHTML = arranger(femaleToilet,sepBool);
+
+  // calcAltItems(maleNum)
+  //console.log(calcAltItems(maleNum))
+//    <p>alternatively, male toilet requires ${maleToiletAlt} WCs, ${maleUrinal} urinals and ${maleWashbasinAlt} washbasins</p>
+}
 
 function arranger(number,disSep){
   let arrangement = '';
@@ -127,67 +154,34 @@ function arranger(number,disSep){
 //       } }
 // }
 
-urinalCheck.addEventListener('change', function() {
+urinalCheck.addEventListener('change', function(event) {
+  event.preventDefault();
   if (urinalCheck.checked) {
     uriBool = true;
     //console.log('uriBool: ' , uriBool)
   } else {
     uriBool = false;
     //console.log('uriBool: ' , uriBool)
-  } });
+  }
+  runCalcs()
+});
 
-separateCheck.addEventListener('change', function() {
+separateCheck.addEventListener('change', function(event) {
+  event.preventDefault();
   if (separateCheck.checked) {
     sepBool = true;
   //console.log('sepBool: ' , sepBool)
   } else {
     sepBool = false;
     //console.log('sepBool: ' , sepBool)
-  } });
+  }
+  runCalcs()
+});
 
 
-calcButton.addEventListener('click', function(){
-  //console.log('clicked calc');
-  maleRatio = maleRatioAtt.value;
-  femaleRatio = femaleRatioAtt.value;
-  occupancy = occNumAtt.value;
-  //console.log(maleRatio)
-  //console.log(femaleRatio)
-  //console.log(occupancy)
-  maleNum = Math.ceil(occupancy * maleRatio / 100);
-  femaleNum = Math.ceil(occupancy * femaleRatio / 100);
-    
-  //console.log(femaleNum)
-  maleToilet = calcItems(maleNum,uriBool)[0];
-  maleUrinal = calcItems(maleNum,uriBool)[1];
-  //console.log(maleToilet)
-  femaleToilet = calcItems(femaleNum)[0];
-  //console.log(femaleToilet)
-  maleWashbasin = calcItems(maleNum,uriBool)[2];
-  femaleWashbasin = calcItems(femaleNum)[2];
-  // maleToiletAlt = calcAltItems(maleNum)[0]
-  // maleWashbasinAlt = calcAltItems(maleNum)[2]
-  // maleUrinal = calcAltItems(maleNum)[1]
-
-    
-    
-    
-  //console.log(femaleToilet)
-
-
-  //console.log(maleNum, femaleNum)
-  resultParagraph.innerHTML = null;
-  resultParagraph.innerHTML = `<p>Based on ${occupancy} occupants number, at ${maleRatio}/${femaleRatio} ratio</p>
-    <p>male occupancy number is: ${maleNum} and female occupancy number is: ${femaleNum}</p>
-    ${uriBool==false ? `<p>male toilet requires ${maleToilet} WCs and ${maleWashbasin} washbasins</p>` : `<p>male toilet requires ${maleToilet} WCs, ${maleUrinal} urinals and ${maleWashbasinAlt} washbasins</p>` }
-    <p>female toilet requires ${femaleToilet} WCs and ${femaleWashbasin} washbasins</p>`;
-
-  arrangeParagraph.innerHTML = null;
-  arrangeParagraph.innerHTML = arranger(maleToilet,sepBool);
-
-  // calcAltItems(maleNum)
-  //console.log(calcAltItems(maleNum))
-//    <p>alternatively, male toilet requires ${maleToiletAlt} WCs, ${maleUrinal} urinals and ${maleWashbasinAlt} washbasins</p>
+calcButton.addEventListener('click', function(event){
+  event.preventDefault();
+  runCalcs()
 });
 
 pdfButton.addEventListener('click', function(){
