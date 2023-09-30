6980f1d060ef16304584a1c9145594fb3c0a227d
Wojtek Nowak <billnewmann@gmail.com>
2023-09-30 16:44:00 +0100

    refactor calcItems() function, and modify listener accordingly

--- src/js/app.js
+++ src/js/app.js
@@ -62,24 +62,25 @@ femaleRatio = defaultRatio
 
 function calcItems(num, uri){
     let result
-    let over100 = 0
+
+    //let over100 = 0
  if (uri == true) {
 
-  result = num<=15 ? [1,1,1] : num<=30 ? [2,1,2] : num<=45 ? [2,2,2] : num<=60 ? [3,2,3] : num<=75 ? [3,3,3] : num<=90 ? [4,3,4] : [4,4,4]
-  over100 = num-100 > 0 ? Math.ceil((num - 100)/50) : 0 
+  result = num<=15 ? [1,1,1] : num<=30 ? [2,1,2] : num<=45 ? [2,2,2] : num<=60 ? [3,2,3] : num<=75 ? [3,3,3] : num<=90 ? [4,3,4] : num<=100 ? [4,4,4] : [4 , 4 + Math.ceil((num - 100)/50), 4 + Math.ceil((num - 100)/50)]
+  //over100 = num-100 > 0 ? Math.ceil((num - 100)/50) : 0 
 
 
 
  } else {
 
-  result = num<=5 ? 1 : num<=15 ? 2 : num<=30 ? 3 : num<=45 ? 4 : num<=60 ? 5: num<=75 ? 6 : num<=90 ? 7 : 8
-  over100 = num-100 > 0 ? Math.ceil((num - 100)/25) : 0 
+  result = num<=5 ? [1,0,1] : num<=15 ? [2,0,2] : num<=30 ? [3,0,3] : num<=45 ? [4,0,4] : num<=60 ? [5,0,5]: num<=75 ? [6,0,6] : num<=90 ? [7,0,7] : num<=100 ? [8,0,8] : [8 + Math.ceil((num - 100)/25),0,8 + Math.ceil((num - 100)/25)]
+  //over100 = num-100 > 0 ? Math.ceil((num - 100)/25) : 0 
 
  }
 
 
     //console.log('result of over: ',over100)
-    result = result + over100
+    //result = result + over100
 return result}
 
 // function calcItems(num){
@@ -91,14 +92,14 @@ return result}
 //   result = result + over100
 // return result}
 
-function calcAltItems(num){
-    let result
-    let over100 = 0
-    result = num<=15 ? [1,1,1] : num<=30 ? [2,1,2] : num<=45 ? [2,2,2] : num<=60 ? [3,2,3] : num<=75 ? [3,3,3] : num<=90 ? [4,3,4] : [4,4,4]
-    over100 = num-100 > 0 ? Math.ceil((num - 100)/50) : 0 
-    //result = result + over100
-    //console.log(num)
-return result}
+// function calcAltItems(num){
+//     let result
+//     let over100 = 0
+//     result = num<=15 ? [1,1,1] : num<=30 ? [2,1,2] : num<=45 ? [2,2,2] : num<=60 ? [3,2,3] : num<=75 ? [3,3,3] : num<=90 ? [4,3,4] : [4,4,4]
+//     over100 = num-100 > 0 ? Math.ceil((num - 100)/50) : 0 
+//     //result = result + over100
+//     //console.log(num)
+// return result}
 
 function arranger(number,disSep){
   let arrangement = ''
@@ -156,12 +157,13 @@ calcButton.addEventListener('click', function(){
     femaleNum = Math.ceil(occupancy * femaleRatio / 100)
     
     //console.log(femaleNum)
-    maleToilet = calcItems(maleNum,uriBool)
+    maleToilet = calcItems(maleNum,uriBool)[0]
+    maleUrinal = calcItems(maleNum,uriBool)[1]
     //console.log(maleToilet)
-    femaleToilet = calcItems(femaleNum)
+    femaleToilet = calcItems(femaleNum)[0]
     //console.log(femaleToilet)
-    maleWashbasin = maleToilet
-    femaleWashbasin = femaleToilet
+    maleWashbasin = calcItems(maleNum,uriBool)[2]
+    femaleWashbasin = calcItems(femaleNum)[2]
     // maleToiletAlt = calcAltItems(maleNum)[0]
     // maleWashbasinAlt = calcAltItems(maleNum)[2]
     // maleUrinal = calcAltItems(maleNum)[1]
@@ -176,13 +178,13 @@ calcButton.addEventListener('click', function(){
     resultParagraph.innerHTML = null
     resultParagraph.innerHTML = `<p>Based on ${occupancy} occupants number, at ${maleRatio}/${femaleRatio} ratio</p>
     <p>male occupancy number is: ${maleNum} and female occupancy number is: ${femaleNum}</p>
-    <p>male toilet requires ${maleToilet} WCs and ${maleWashbasin} washbasins</p>
+    ${uriBool==false ? `<p>male toilet requires ${maleToilet} WCs and ${maleWashbasin} washbasins</p>` : `<p>male toilet requires ${maleToilet} WCs, ${maleUrinal} urinals and ${maleWashbasinAlt} washbasins</p>` }
     <p>female toilet requires ${femaleToilet} WCs and ${femaleWashbasin} washbasins</p>`
 
     arrangeParagraph.innerHTML = null
     arrangeParagraph.innerHTML = arranger(maleToilet,sepBool)
 
-    calcAltItems(maleNum)
+   // calcAltItems(maleNum)
     //console.log(calcAltItems(maleNum))
 //    <p>alternatively, male toilet requires ${maleToiletAlt} WCs, ${maleUrinal} urinals and ${maleWashbasinAlt} washbasins</p>
   });
