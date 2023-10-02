e5775ac2e49c312766423d05a569d3864dcafd64
Wojtek Nowak <billnewmann@gmail.com>
2023-10-02 23:05:36 +0100

    install prettier and run formatter on project files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
-    }
-}
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
+};
--- src/js/app.js
+++ src/js/app.js
@@ -4,21 +4,19 @@
 
 //ADD BUTTONS
 
-const calcButton = document.getElementById('calcButton');
-const pdfButton = document.getElementById('pdfButton');
+const calcButton = document.getElementById("calcButton");
+const pdfButton = document.getElementById("pdfButton");
 
-const urinalCheck = document.getElementById('urinalCheck');
-const separateCheck = document.getElementById('separateCheck');
-const controlsDiv = document.getElementById('controls');
+const urinalCheck = document.getElementById("urinalCheck");
+const separateCheck = document.getElementById("separateCheck");
+const controlsDiv = document.getElementById("controls");
 
-const maleRatioAtt = document.getElementById('maleRatio');
-const femaleRatioAtt = document.getElementById('femaleRatio');
-const occNumAtt = document.getElementById('occNum');
-const resultParagraph = document.getElementById('resultsPara');
-const arrangeParagraphMale = document.getElementById('arrangeMale');
-const arrangeParagraphFemale = document.getElementById('arrangeFemale');
-
-
+const maleRatioAtt = document.getElementById("maleRatio");
+const femaleRatioAtt = document.getElementById("femaleRatio");
+const occNumAtt = document.getElementById("occNum");
+const resultParagraph = document.getElementById("resultsPara");
+const arrangeParagraphMale = document.getElementById("arrangeMale");
+const arrangeParagraphFemale = document.getElementById("arrangeFemale");
 
 //ADD OCCUPANCYCALCULATOR
 
@@ -26,7 +24,6 @@ let occupancy;
 occupancy = 100;
 
 //occupancy = prompt()
-
 
 //ADD MALENUM AND FEMALENUM VARIABLES
 
@@ -50,32 +47,61 @@ femaleRatio = defaultRatio;
 //maleRatio = prompt()
 //femaleRatio = prompt()
 
-
-
 // console.log('occupancy is: ', occupancy)
 // console.log('ratios are: Male(',maleRatio,') and Female(', femaleRatio, ')')
 // console.log('maleNum is: ',maleNum)
 // console.log('femaleNum is: ',femaleNum)
 
-
-
 //ADD TOILETCALCULATOR
 
-function calcItems(num, uri){
+function calcItems(num, uri) {
   let result;
 
   if (uri == true) {
-    result = num<=15 ? [1,1,1] : num<=30 ? [2,1,2] : num<=45 ? [2,2,2] : num<=60 ? [3,2,3] : num<=75 ? [3,3,3] : num<=90 ? [4,3,4] : num<=100 ? [4,4,4] : [4 , 4 + Math.ceil((num - 100)/50), 4 + Math.ceil((num - 100)/50)];
+    result =
+      num <= 15
+        ? [1, 1, 1]
+        : num <= 30
+        ? [2, 1, 2]
+        : num <= 45
+        ? [2, 2, 2]
+        : num <= 60
+        ? [3, 2, 3]
+        : num <= 75
+        ? [3, 3, 3]
+        : num <= 90
+        ? [4, 3, 4]
+        : num <= 100
+        ? [4, 4, 4]
+        : [4, 4 + Math.ceil((num - 100) / 50), 4 + Math.ceil((num - 100) / 50)];
   } else {
-    result = num<=5 ? [1,0,1] : num<=15 ? [2,0,2] : num<=30 ? [3,0,3] : num<=45 ? [4,0,4] : num<=60 ? [5,0,5]: num<=75 ? [6,0,6] : num<=90 ? [7,0,7] : num<=100 ? [8,0,8] : [8 + Math.ceil((num - 100)/25),0,8 + Math.ceil((num - 100)/25)];
+    result =
+      num <= 5
+        ? [1, 0, 1]
+        : num <= 15
+        ? [2, 0, 2]
+        : num <= 30
+        ? [3, 0, 3]
+        : num <= 45
+        ? [4, 0, 4]
+        : num <= 60
+        ? [5, 0, 5]
+        : num <= 75
+        ? [6, 0, 6]
+        : num <= 90
+        ? [7, 0, 7]
+        : num <= 100
+        ? [8, 0, 8]
+        : [8 + Math.ceil((num - 100) / 25), 0, 8 + Math.ceil((num - 100) / 25)];
   }
-  return result;}
+  return result;
+}
 
 // function calcItems(num){
 //   let result
 //   let over100 = 0
 //   result = num<=5 ? 1 : num<=15 ? 2 : num<=30 ? 3 : num<=45 ? 4 : num<=60 ? 5: num<=75 ? 6 : num<=90 ? 7 : 8
-//   over100 = num-100 > 0 ? Math.ceil((num - 100)/25) : 0 
+//   over100 = num-100 > 0 ? Math.ceil((num - 100)/25) : 0
 //   //console.log('result of over: ',over100)
 //   result = result + over100
 // return result}
@@ -84,66 +110,83 @@ function calcItems(num, uri){
 //     let result
 //     let over100 = 0
 //     result = num<=15 ? [1,1,1] : num<=30 ? [2,1,2] : num<=45 ? [2,2,2] : num<=60 ? [3,2,3] : num<=75 ? [3,3,3] : num<=90 ? [4,3,4] : [4,4,4]
-//     over100 = num-100 > 0 ? Math.ceil((num - 100)/50) : 0 
+//     over100 = num-100 > 0 ? Math.ceil((num - 100)/50) : 0
 //     //result = result + over100
 //     //console.log(num)
 // return result}
 
-function runCalcs(){
-
+function runCalcs() {
   maleRatio = maleRatioAtt.value;
   femaleRatio = femaleRatioAtt.value;
   occupancy = occNumAtt.value;
-  maleNum = Math.ceil(occupancy * maleRatio / 100);
-  femaleNum = Math.ceil(occupancy * femaleRatio / 100);
-  maleToilet = calcItems(maleNum,uriBool)[0];
-  maleUrinal = calcItems(maleNum,uriBool)[1];
+  maleNum = Math.ceil((occupancy * maleRatio) / 100);
+  femaleNum = Math.ceil((occupancy * femaleRatio) / 100);
+  maleToilet = calcItems(maleNum, uriBool)[0];
+  maleUrinal = calcItems(maleNum, uriBool)[1];
   femaleToilet = calcItems(femaleNum)[0];
-  maleWashbasin = calcItems(maleNum,uriBool)[2];
+  maleWashbasin = calcItems(maleNum, uriBool)[2];
   femaleWashbasin = calcItems(femaleNum)[2];
   // maleToiletAlt = calcAltItems(maleNum)[0]
   // maleWashbasinAlt = calcAltItems(maleNum)[2]
   // maleUrinal = calcAltItems(maleNum)[1]
 
-    
-    
-    
   //console.log(femaleToilet)
-
 
   //console.log(maleNum, femaleNum)
   resultParagraph.innerHTML = null;
   resultParagraph.innerHTML = `<p>Based on ${occupancy} occupants number, at ${maleRatio}/${femaleRatio} ratio</p>
     <p>male occupancy number is: ${maleNum} and female occupancy number is: ${femaleNum}</p>
-    ${uriBool==false ? `<p>male toilet requires ${maleToilet} WCs and ${maleWashbasin} washbasins</p>` : `<p>male toilet requires ${maleToilet} WCs, ${maleUrinal} urinals and ${maleWashbasin} washbasins</p>` }
+    ${
+      uriBool == false
+        ? `<p>male toilet requires ${maleToilet} WCs and ${maleWashbasin} washbasins</p>`
+        : `<p>male toilet requires ${maleToilet} WCs, ${maleUrinal} urinals and ${maleWashbasin} washbasins</p>`
+    }
     <p>female toilet requires ${femaleToilet} WCs and ${femaleWashbasin} washbasins</p>`;
 
   arrangeParagraphMale.innerHTML = null;
   arrangeParagraphFemale.innerHTML = null;
-  arrangeParagraphMale.innerHTML = arranger(maleToilet,sepBool);
-  arrangeParagraphFemale.innerHTML = arranger(femaleToilet,sepBool);
+  arrangeParagraphMale.innerHTML = arranger(maleToilet, sepBool);
+  arrangeParagraphFemale.innerHTML = arranger(femaleToilet, sepBool);
 
   // calcAltItems(maleNum)
   //console.log(calcAltItems(maleNum))
-//    <p>alternatively, male toilet requires ${maleToiletAlt} WCs, ${maleUrinal} urinals and ${maleWashbasinAlt} washbasins</p>
+  //    <p>alternatively, male toilet requires ${maleToiletAlt} WCs, ${maleUrinal} urinals and ${maleWashbasinAlt} washbasins</p>
 }
 
-function arranger(number,disSep){
-  let arrangement = '';
+function arranger(number, disSep) {
+  let arrangement = "";
   for (let i = 1; i <= number; i++) {
-    if (disSep == false){
+    if (disSep == false) {
       arrangement = arrangement.concat(`<p>cubicle ${i}: `);
-      arrangement = i==1 ? arrangement.concat(`disabled accessible toilet</p> `) : i==2 ? arrangement.concat(`ambulant disabled</p> `) : i==3 ? arrangement.concat(`normal cubicle</p> `) : i==4 ? arrangement.concat(`enlarged cubicle</p> `) : arrangement.concat(`normal cubicle</p> `);
+      arrangement =
+        i == 1
+          ? arrangement.concat(`disabled accessible toilet</p> `)
+          : i == 2
+          ? arrangement.concat(`ambulant disabled</p> `)
+          : i == 3
+          ? arrangement.concat(`normal cubicle</p> `)
+          : i == 4
+          ? arrangement.concat(`enlarged cubicle</p> `)
+          : arrangement.concat(`normal cubicle</p> `);
     } else {
-
-
-      arrangement = i==1 ? arrangement.concat(`<p>separate cubicle: `) : arrangement.concat(`<p>cubicle ${i-1}: `);
-      arrangement = i==1 ? arrangement.concat(`disabled accessible toilet</p> `) : i==2 ? arrangement.concat(`ambulant disabled</p> `) : 3<=i<=4 ? arrangement.concat(`normal cubicle</p> `) : i==5 ? arrangement.concat(`enlarged cubicle</p> `) : arrangement.concat(`normal cubicle</p> `);
+      arrangement =
+        i == 1
+          ? arrangement.concat(`<p>separate cubicle: `)
+          : arrangement.concat(`<p>cubicle ${i - 1}: `);
+      arrangement =
+        i == 1
+          ? arrangement.concat(`disabled accessible toilet</p> `)
+          : i == 2
+          ? arrangement.concat(`ambulant disabled</p> `)
+          : 3 <= i <= 4
+          ? arrangement.concat(`normal cubicle</p> `)
+          : i == 5
+          ? arrangement.concat(`enlarged cubicle</p> `)
+          : arrangement.concat(`normal cubicle</p> `);
     }
   }
   return arrangement;
 }
-
 
 // function toggler(funcName, boolName){
 //   if (urinalCheck.checked) {
@@ -155,7 +198,7 @@ function arranger(number,disSep){
 //       } }
 // }
 
-urinalCheck.addEventListener('change', function(event) {
+urinalCheck.addEventListener("change", function (event) {
   event.preventDefault();
   if (urinalCheck.checked) {
     uriBool = true;
@@ -164,34 +207,34 @@ urinalCheck.addEventListener('change', function(event) {
     uriBool = false;
     //console.log('uriBool: ' , uriBool)
   }
-  runCalcs()
+  runCalcs();
 });
 
-separateCheck.addEventListener('change', function(event) {
+separateCheck.addEventListener("change", function (event) {
   event.preventDefault();
   if (separateCheck.checked) {
     sepBool = true;
-  //console.log('sepBool: ' , sepBool)
+    //console.log('sepBool: ' , sepBool)
   } else {
     sepBool = false;
     //console.log('sepBool: ' , sepBool)
   }
-  runCalcs()
+  runCalcs();
 });
 
-controlsDiv.addEventListener('change', (e) => {
-  if(e.target.type = 'number') {
-    runCalcs()
+controlsDiv.addEventListener("change", (e) => {
+  if ((e.target.type = "number")) {
+    runCalcs();
   }
 });
 
-calcButton.addEventListener('click', function(event){
+calcButton.addEventListener("click", function (event) {
   event.preventDefault();
-  runCalcs()
+  runCalcs();
 });
 
-pdfButton.addEventListener('click', function(){
-  console.log('clicked pdf');
+pdfButton.addEventListener("click", function () {
+  console.log("clicked pdf");
 });
 
 //ADD BLOCKARRANGER
